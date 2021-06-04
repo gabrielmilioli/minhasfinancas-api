0c39823e7c65461897283f5c8dfdf75940c5fd10
Gabriel <gmilioli@outlook.com>
2021-06-03 21:52:09 -0300

    Criada configuração de banco de dados, entidades, enums e repositórios

---
+++ src/main/java/com/milioli/minhasfinancas/model/enums/TipoLancamento.java
@@ -0,0 +1,6 @@
+package com.milioli.minhasfinancas.model.enums;
+
+public enum TipoLancamento {
+	RECEITA,
+	DESPESA
+}
---
+++ src/main/java/com/milioli/minhasfinancas/model/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.milioli.minhasfinancas.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.milioli.minhasfinancas.model.entity.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
