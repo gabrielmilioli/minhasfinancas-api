4f0ce7d71fff498c16548aab170481491fd34810
Gabriel <gmilioli@outlook.com>
2021-06-21 12:51:05 -0300

    Sqls executados

---
+++ src/postgresql/1/cria_schema_e_tabelas.sql
@@ -0,0 +1,25 @@
+create schema financas;
+
+create table financas.usuario
+(
+    id bigint not null primary key,
+    nome character varying(150),
+    email character varying(100),
+    senha character varying(20),
+    data_cadastro date default now()
+);
+
+create table financas.lancamento
+(
+    id bigint not null primary key,
+    descricao character varying(100) not null,
+    mes integer not null,
+    ano integer not null,
+    valor numeric(16,2) not null,
+    tipo character varying(20) not null,
+    status character varying(20) not null,
+    id_usuario bigint references financas.usuario (id),
+    data_cadastro date default now(),
+    constraint lancamento_tipo_check check (tipo::text = any (array['RECEITA'::character varying, 'DESPESA'::character varying]::text[])),
+    constraint lancamento_status_check check (status::text = any (array['PENDENTE'::character varying, 'CANCELADO'::character varying, 'EFETIVADO'::character varying]::text[]))
+);
